61a750510675fba7dbfa53379663541aea2b4ad8
Guilherme Cardoso <gcardoso18@gmail.com>
2017-11-16 22:37:15 -0200

    Adding tests

--- app/src/test/java/com/guilhermecardoso/shotstest/modules/shotdetails/ShotDetailsPresenterTest.java
+++ app/src/test/java/com/guilhermecardoso/shotstest/modules/shotdetails/ShotDetailsPresenterTest.java
@@ -1,8 +1,101 @@
 package com.guilhermecardoso.shotstest.modules.shotdetails;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.concurrent.Callable;
+
+import app.modules.shotdetails.ShotDetailsContract;
+import app.modules.shotdetails.ShotDetailsPresenter;
+import domain.entities.Shot;
+import domain.network.ShotsAPIService;
+import io.reactivex.Scheduler;
+import io.reactivex.Single;
+import io.reactivex.android.plugins.RxAndroidPlugins;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.functions.Function;
+import io.reactivex.plugins.RxJavaPlugins;
+import io.reactivex.schedulers.Schedulers;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 /**
  * Created by guilhermecardoso on 11/16/17.
  */
 
 public class ShotDetailsPresenterTest {
+
+    private ShotDetailsPresenter presenter;
+    @Mock
+    private ShotDetailsContract.View mockedView;
+    @Mock private ShotsAPIService mockedService;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+
+        presenter = new ShotDetailsPresenter(mockedView, new CompositeDisposable(), mockedService);
+    }
+
+    @Before
+    public void setupClass() {
+        RxAndroidPlugins.setInitMainThreadSchedulerHandler(
+                new Function<Callable<Scheduler>, Scheduler>() {
+                    @Override
+                    public Scheduler apply(Callable<Scheduler> schedulerCallable) throws Exception {
+                        return Schedulers.newThread();
+                    }
+                }
+        );
+        RxJavaPlugins.setNewThreadSchedulerHandler(new Function<Scheduler, Scheduler>() {
+            @Override
+            public Scheduler apply(Scheduler scheduler) throws Exception {
+                return Schedulers.trampoline();
+            }
+        });
+    }
+
+    @After
+    public void cleanUp() {
+//        presenter = null;
+//        RxAndroidPlugins.reset();
+        Schedulers.shutdown();
+    }
+
+    @Test
+    public void shouldPresentDetails() {
+        long id = 1;
+        mockServiceResponse();
+
+        presenter.loadShotDetails(id);
+
+        verify(mockedView).showLoading();
+        verify(mockedView).showShotDetails(any(Shot.class));
+        verify(mockedView).hideLoading();
+    }
+
+    @Test
+    public void shouldPresentError() {
+        long id = 1;
+        mockServiceError();
+
+        presenter.loadShotDetails(id);
+
+        verify(mockedView).showLoading();
+        verify(mockedView).showError();
+        verify(mockedView).hideLoading();
+    }
+
+    private void mockServiceError() {
+        when(mockedService.getShot(any(Long.class))).thenReturn(Single.<Shot>error(new NullPointerException()));
+    }
+
+    private void mockServiceResponse() {
+        when(mockedService.getShot(any(Long.class))).thenReturn(Single.<Shot>just(new Shot()));
+    }
 }
--- app/src/test/java/com/guilhermecardoso/shotstest/modules/shotslist/ShotsListPresenterTest.java
+++ app/src/test/java/com/guilhermecardoso/shotstest/modules/shotslist/ShotsListPresenterTest.java
@@ -2,21 +2,29 @@ package com.guilhermecardoso.shotstest.modules.shotslist;
 
 import org.junit.After;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.Callable;
 
 import app.modules.shotslist.ShotsListContract;
 import app.modules.shotslist.ShotsListPresenter;
+import domain.entities.Shot;
 import domain.network.ShotsAPIService;
+import io.reactivex.Observable;
 import io.reactivex.Scheduler;
 import io.reactivex.android.plugins.RxAndroidPlugins;
+import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.functions.Function;
+import io.reactivex.plugins.RxJavaPlugins;
 import io.reactivex.schedulers.Schedulers;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 /**
  * Created by guilhermecardoso on 11/16/17.
@@ -25,45 +33,69 @@ import io.reactivex.schedulers.Schedulers;
 public class ShotsListPresenterTest {
 
     private ShotsListPresenter presenter;
-    @Mock private ShotsListContract.View view;
-//    @Mock private CompositeDisposable sCompositeDisposable;
-    @Mock private ShotsAPIService service;
-
-
+    @Mock
+    private ShotsListContract.View mockedView;
+    private CompositeDisposable mockedCompositeDisposable;
+    @Mock private ShotsAPIService mockedService;
 
     @Before
     public void setUp() throws Exception {
         MockitoAnnotations.initMocks(this);
-        presenter = new ShotsListPresenter(view);
 
+        presenter = new ShotsListPresenter(mockedView, new CompositeDisposable(), mockedService);
     }
 
 
-    @BeforeClass
-    public static void setupClass() {
+    @Before
+    public void setupClass() {
         RxAndroidPlugins.setInitMainThreadSchedulerHandler(
             new Function<Callable<Scheduler>, Scheduler>() {
                @Override
                public Scheduler apply(Callable<Scheduler> schedulerCallable) throws Exception {
-                   return Schedulers.trampoline();
+                   return Schedulers.newThread();
                }
            }
         );
+        RxJavaPlugins.setNewThreadSchedulerHandler(new Function<Scheduler, Scheduler>() {
+            @Override
+            public Scheduler apply(Scheduler scheduler) throws Exception {
+                return Schedulers.trampoline();
+            }
+        });
     }
 
     @After
     public void tearDown() {
         RxAndroidPlugins.reset();
+        presenter = null;
     }
 
     @Test
-    public void shouldLoadShot() {
-        //Given a page
-        int page = 1;
+    public void shouldLoadShotDetails() {
+        //Given a shot ID
+        int shotID = 1;
 
-        //When we try to fetch
-        presenter.onShotClicked(page);
-        //We should ask the view to show it
-        Mockito.verify(view).showShotDetails(Mockito.any(Long.class));
+        //When we try to load it
+        presenter.onShotClicked(shotID);
+        //We should ask the mockedView to show it
+        verify(mockedView).showShotDetails(any(Long.class));
     }
+
+    @Test
+    public void shouldFetchShots() {
+        //Given a page and a successful completable
+        int page = 1;
+        mockServiceResponse();
+
+        //When we ask the presenter to load it
+        presenter.fetchPage(page);
+
+        //The view should be asked to show it
+        verify(mockedView).showShots(any(List.class));
+    }
+
+    private void mockServiceResponse() {
+        when(mockedService.listShots(any(Integer.class))).thenReturn(Observable.<List<Shot>>just(new ArrayList<Shot>()));
+    }
+
 }
